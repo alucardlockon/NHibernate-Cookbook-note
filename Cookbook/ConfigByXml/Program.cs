068a801c0d86559cbd6bc0cf122eb7d7e74d21fa
alucardlockon <alucardlockon3@gmail.com>
2017-10-17 08:50:33 +0800

    add sln

---
+++ Cookbook/ConfigByXml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConfigByXml
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Cookbook/Cookbook/TestClass.cs
@@ -0,0 +1,8 @@
+﻿namespace Cookbook
+{
+    public class TestClass
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
