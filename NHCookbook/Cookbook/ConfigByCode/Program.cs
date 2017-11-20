1061d88771735135f79f8b46f602132ac8a2a1ed
alucardlockon <alucardlockon3@gmail.com>
2017-11-20 11:38:43 +0800

    1.2-1.5

---
+++ NHCookbook/Cookbook/ConfigByCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+
+namespace ConfigByCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nhConfig = new Configuration().DataBaseIntegration(db =>
+            {
+                db.Dialect<MsSql2012Dialect>();
+                db.ConnectionStringName = "db";
+                db.BatchSize = 100;
+            });
+            var sessionFactory = nhConfig.BuildSessionFactory();
+            Console.WriteLine("NHibernate Configured!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NHCookbook/Cookbook/ConfigByFNH/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace ConfigByFNH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = MsSqlConfiguration.MsSql2012
+ .ConnectionString(connstr => connstr.FromConnectionStringWithKey("db"))
+ .AdoNetBatchSize(100);
+            var nhConfig = Fluently.Configure()
+             .Database(config)
+             .BuildConfiguration();
+            var sessionFactory = nhConfig.BuildSessionFactory();
+            Console.WriteLine("NHibernate configured fluently!");
+            Console.ReadKey();
+        }
+    }
+}
