0a800604c481d3a96fcaf6ac0a5aa2b956f0d4fe
alucardlockon <alucardlockon3@gmail.com>
2017-11-27 09:55:03 +0800

    2-2

---
+++ NHCookbook/Cookbook/Eg.Core/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eg.Core
+{
+    public class Movie : Product
+    {
+
+        public virtual string Director { get; set; }
+
+    }
+}
